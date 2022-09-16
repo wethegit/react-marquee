ae51fe67bbe854589d730a58ff6f90edf14f16db
chanroyc <chanroyc@gmail.com>
2022-09-16 16:57:59 -0700

    update(webpack): remove style-loader testing

--- webpack.config.js
+++ webpack.config.js
@@ -20,16 +20,8 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
           'css-loader',
           'sass-loader',
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
         ],
       },
       {
