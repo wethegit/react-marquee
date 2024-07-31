97d9082616beabac7a257fae50a60e9266ac1658
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-07-30 17:12:09 -0700

    feat: add runtime and react-dom to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,14 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
         },
       },
     },
