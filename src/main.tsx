fab2955346d2fbe05119c2f2502c813da5bc8b4c
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-07-31 09:49:48 -0700

    fix: type error on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -29,10 +29,7 @@ function Slide() {
   )
 }
 
-const container = document.getElementById("root")
-const root = createRoot(container)
-
-root.render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
